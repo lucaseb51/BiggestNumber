740c4800f41df387c742933e34e8cec3e2f7ed70
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-02-06 18:17:21 -0800

    Takes in user input

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,12 +5,16 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+int number1;
+int number2;
 
+
+  cout<<"Please enter a whole number:\n";
+  cin>>number1;
+  cout<<"Please enter another whole number:\n";
+  cin>>number2;
   cout<<"Of those two numbers, the biggest is: ";
+
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
