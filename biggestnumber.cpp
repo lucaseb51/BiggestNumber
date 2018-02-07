3a4f693cba4e812b7f11c6e27077c8b8e1422694
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-02-06 18:23:26 -0800

    Distinguishes the biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Lucas Eastburn
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
@@ -7,13 +7,20 @@ int main()
 {
 int number1;
 int number2;
-
+int biggest;
 
   cout<<"Please enter a whole number:\n";
   cin>>number1;
   cout<<"Please enter another whole number:\n";
   cin>>number2;
-  cout<<"Of those two numbers, the biggest is: ";
+  
+  if(number1>number2){
+  biggest=number1;
+  }else{
+  biggest=number2;
+  }
+
+  cout<<"Of those two numbers, the biggest is: "<<biggest;
 
   cout<<endl<<"Thank you for playing.\n";
 
